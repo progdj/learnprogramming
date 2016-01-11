ec3ed32f68b6e1c9208b005f6a0ed473cb9db3eb
Jens Radkte <swefl@fin-sn.de>
2016-01-11 13:15:31 +0000

    add vhost cms.vrs.local for amak-cms
    git-svn-id: svn://svn.vrsmedia.de/amak/entwicklungsumgebung/amak-docker@7456 785b5691-6530-e34c-ac92-580984ead99b

--- app/paths.php
+++ app/paths.php
@@ -1,9 +1,10 @@
 <?php
 
-// What's that?
-// $backend_upload_tmp = 'D:/amak/entwicklungsumgebung/amak-cms/upload_tmp/';
+// What's that? - Seems to be used in context of licenseholder creation (found in cms backend)
+$backend_upload_tmp = '/var/www/amak-cms/upload_tmp/';
 
-$frontend_data_path = '/var/www/amak-frontend/';
+// What's that? - Seems to be used in context of licenseholder creation (found in cms backend)
+$frontend_data_path = '/var/www/amak-frontend/data/';
 
 // What's that?
 // $frontend_data_init_path = 'D:/amak/entwicklungsumgebung/amak-cms/protected/config/';
--- app/run.sh
+++ app/run.sh
@@ -2,18 +2,26 @@
 
 echo AMAK Data Container initialized
 
-DIR_PROJECT=/var/www/amak-frontend
-DIR_CONFIG=$DIR_PROJECT/protected/config
-DIR_HTML2PDF=$DIR_PROJECT/protected/vendor/html2pdf/html2pdf
+AMAK_DIR_PROJECT=/var/www/amak-frontend
+AMAK_DIR_CONFIG=$AMAK_DIR_PROJECT/protected/config
+AMAK_DIR_HTML2PDF=$AMAK_DIR_PROJECT/protected/vendor/html2pdf/html2pdf
 
+CMS_DIR_PROJECT=/var/www/amak-cms
+CMS_DIR_CONFIG=$CMS_DIR_PROJECT/protected/config
 # Check if project files are deployed
-if [ -d $DIR_CONFIG ]; then
-    echo Copying project configuration files...
+if [ -d $AMAK_DIR_CONFIG ]; then
+    echo Copying project configuration files for amak...
 
-    cp /db_local.php $DIR_CONFIG
-    cp /paths.php $DIR_CONFIG
-    cp /html2pdf.config.php $DIR_HTML2PDF/config.inc.php
+    cp /db_local.php $AMAK_DIR_CONFIG
+    cp /paths.php $AMAK_DIR_CONFIG
+    cp /html2pdf.config.php $AMAK_DIR_HTML2PDF/config.inc.php
+    
+    mkdir $AMAK_DIR_PROJECT/protected/runtime/ -p
+    chmod 777 $AMAK_DIR_PROJECT/protected/runtime/
 
+    mkdir $AMAK_DIR_PROJECT/assets/ -p
+    chmod 777 $AMAK_DIR_PROJECT/assets/
+    
     echo Done.
 else
     >&2 echo ERROR: No project files found!
@@ -22,4 +30,21 @@ else
     mkdir /var/www/amak-frontend/img > /dev/null
     cp /default/* /var/www/amak-frontend
     cp /default/img/* /var/www/amak-frontend/img
+    
+fi
+
+
+if [ -d $CMS_DIR_CONFIG ]; then
+    echo Copying project configuration files for cms...
+
+    cp /db_local.php $CMS_DIR_CONFIG
+    cp /paths.php $CMS_DIR_CONFIG
+    
+    mkdir $CMS_DIR_PROJECT/protected/runtime/ -p
+    chmod 777 $CMS_DIR_PROJECT/protected/runtime/
+    
+    mkdir $CMS_DIR_PROJECT/assets/ -p
+    chmod 777 $CMS_DIR_PROJECT/assets/
+    
+    echo Done.
 fi
