13374392c9c16ca86a66fbcd390451f3c508ab62
Jens Radtke <swefl@fin-sn.de>
2016-05-12 21:50:54 +0200

    upgrade image magick

--- httpd/compile/01-imagemagick.sh
+++ httpd/compile/01-imagemagick.sh
@@ -5,9 +5,9 @@ cd /compile/
 # Compile and install script for imagemagick version 6.9.3-10.
 
 apt-get update && apt-get install build-essential && apt-get build-dep imagemagick -y
-wget http://www.imagemagick.org/download/ImageMagick-6.9.3-10.tar.gz
-tar xzvf ImageMagick-6.9.3-10.tar.gz
-cd ImageMagick-6.9.3-10/
+wget http://www.imagemagick.org/download/ImageMagick-6.9.4-1.tar.gz
+tar xzvf ImageMagick-6.9.4-1.tar.gz
+cd ImageMagick-6.9.4-1/
 ./configure
 make
 make install
