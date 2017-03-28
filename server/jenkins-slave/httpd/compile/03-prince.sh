a98746a312b96af5354f0630f12a2fb2f21b3409
Steffen Jäckel <jaeckel@vrsmedia.de>
2017-03-28 11:35:53 +0200

    Install license-key for PrinceXml Pdf-Generation

--- server/jenkins-slave/httpd/compile/03-prince.sh
+++ server/jenkins-slave/httpd/compile/03-prince.sh
@@ -11,7 +11,11 @@ tar xzvf prince-11.1-ubuntu14.04-amd64.tar.gz
 
 cd prince-11.1-ubuntu14.04-amd64/
 
-./install.sh < "/usr/bin"
+./install.sh < "/usr/local/bin"
+
+cd ..
+
+cp ./prince-11-license.dat /usr/local/lib/prince/license/license.dat
 
 echo "installed prince 11.1"
 
